4ff6ceb0649edde874e05b304d41b67af1469faa
Rognorek <80618369+Rognorek@users.noreply.github.com>
2021-05-05 17:28:05 +0700

    Add files via upload

---
+++ More.Problem05.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KGL.DEVCS.MORE.PR05
+{
+    class Program
+    {
+        static void Main()
+        {
+            do
+            {
+                Console.Clear();
+                Proc();
+                Console.WriteLine("Repeat?");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+        static void Proc()
+        {
+            int usernumber, counter = 1;
+            List<int> userlist = new();
+
+            do
+            {
+                Console.WriteLine("Введите {0} элемент последовательности: ", counter);
+                if (int.TryParse(Console.ReadLine(), out usernumber))
+                {
+                    userlist.Add(usernumber);
+                    counter += 1;
+                    if (usernumber < 0) break;
+                }
+            } while (true);
+
+            Console.Clear();
+            Console.WriteLine("Для списка: ");
+            foreach (int item in userlist)
+            {
+                Console.Write(item + " ");
+            }
+
+            counter = 0;
+            if (userlist.Count > 2)
+            {
+                for (int i = 1; i < userlist.Count - 1; i++)
+                {
+                    if (userlist[i - 1] < userlist[i] && userlist[i + 1] < userlist[i]) counter++;
+                }
+            }
+            Console.WriteLine("Ответ: {0}", counter);
+        }
+    }
+}
